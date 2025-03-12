5ae249fd0eb1ebee83f282525a77dd6cbb7ea247
talha-fullstackdev <talhafullstack786@gmail.com>
2025-03-12 12:00:03 -0800

    updated

--- db/connectiondb.js
+++ db/connectiondb.js
@@ -4,9 +4,9 @@ const connectionDB = async () => {
     await mongoose.connect(
       "mongodb+srv://nawazt964:yDrkUaTtFMJ64euq@cluster0.7lsnp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     );
-    console.log("data base connected succesfully");
+    console.log("Data base connected succesfully");
   } catch (err) {
-    console.error("Error occured while connecting to data base",err);
+    console.error("Error occured while connecting to data base!",err);
   }
 };
 export default connectionDB;
